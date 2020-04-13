e651976d1837f4e761a5f64e5a66e97117e65bb7
u1065206 <u10155566@gmail.com>
2020-04-13 11:45:40 +0800

    0413

---
+++ app/src/main/java/com/example/u1065206_0413/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.u1065206_0413;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        CheckBox chk;
+        TextView txvShow;
+        String msg = "";
+        int[] id = {R.id.chkMina,R.id.chkMomo,R.id.chkSana,R.id.chkTzuyu};
+        for(int i:id){
+            chk=findViewById(i);
+            if(chk.isChecked())
+                msg +="\n" +chk.getText();
+        }
+        txvShow=findViewById(R.id.txvShow);
+
+        txvShow.setText(msg);
+    }
+}
